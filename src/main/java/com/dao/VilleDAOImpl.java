6d36844f29f0aaa35738cd3efed6dfb0af0851ca
p1 <guideb21@orange.fr>
2020-06-20 14:26:44 +0200

    opti dupli

--- src/main/java/com/dao/VilleDAOImpl.java
+++ src/main/java/com/dao/VilleDAOImpl.java
@@ -21,11 +21,137 @@ public class VilleDAOImpl implements VilleDAO {
 	private static final String[] ATTRIBUTS_VILLE = { "Code_commune_INSEE", "Nom_commune", "Code_postal", "Latitude",
 			"Longitude", "Libelle_acheminement", "Ligne_5" };
 
-	@SuppressWarnings("deprecation")
 	public ArrayList<Ville> findAllVilles() {
-		ArrayList<Ville> villes = new ArrayList<Ville>();
-
 		String requete = "SELECT * FROM ville_france";
+		ArrayList<Ville> villes = getVilleWithRequete(requete);
+		return villes;
+	}
+
+	@Override
+	public ArrayList<Ville> findSomeVilles(Ville ville) {
+		String requete = "SELECT * FROM ville_france";
+		String requeteWhere = " WHERE ";
+		String[][] attributs = { ATTRIBUTS_VILLE,
+				{ ville.getCodeCommune(), ville.getNomCommune(), ville.getCodePostal(), ville.getLatitude(),
+						ville.getLongitude(), ville.getLibelleAcheminement(), ville.getLigne_5() } };
+
+		// Creation String condition
+		boolean conditionSQL = false;
+		for (int i = 0; i < attributs[0].length; i++) {
+			if (attributs[1][i] != null) {
+				if (conditionSQL) {
+					requeteWhere += "AND ";
+				}
+				requeteWhere += attributs[0][i] + "='" + attributs[1][i] + "' ";
+				conditionSQL = true;
+			}
+		}
+
+		// Assemblage des Strings requete
+		String requeteFinale = requete;
+		if (conditionSQL) {
+			requeteFinale += requeteWhere;
+		}
+
+		ArrayList<Ville> villes = getVilleWithRequete(requeteFinale);
+
+		return villes;
+	}
+
+	@Override
+	public boolean addVille(Ville ville) {
+
+		String requete = "INSERT INTO ville_france (";
+
+		for (String attribut : ATTRIBUTS_VILLE) {
+			requete += "" + attribut + ",";
+		}
+		requete = requete.substring(0, requete.length() - 1);
+		requete += ") VALUES (";
+
+		if (ville.getCodeCommune() != null) {
+			requete += "'" + ville.getCodeCommune() + "',";
+		} else {
+			requete += "'NULL" + Math.round(10000 * Math.random()) + "',"; // En tant que clef primaire on ne peut pas
+																			// donner la même valeur à chaque fois.
+		}
+
+		requete += checkNull(ville.getNomCommune());
+		requete += checkNull(ville.getCodePostal());
+		requete += checkNull(ville.getLatitude());
+		requete += checkNull(ville.getLongitude());
+		requete += checkNull(ville.getLibelleAcheminement());
+		requete += checkNull(ville.getLigne_5());
+
+		requete = requete.substring(0, requete.length() - 1);
+		requete += ")";
+
+		return executeRequete(requete);
+	}
+
+	// Modifie la ville dont le numero de commune est donné dans la ville en
+	// parametre et lui donnes les autres parametres donnés dans la ville.
+
+	@Override
+	public boolean modifVille(Ville ville) {
+		String requete = "UPDATE ville_france SET ";
+		String requeteWhere = " WHERE Code_commune_INSEE = '" + ville.getCodeCommune() + "'";
+
+		String[][] attributs = { ATTRIBUTS_VILLE,
+				{ ville.getCodeCommune(), ville.getNomCommune(), ville.getCodePostal(), ville.getLatitude(),
+						ville.getLongitude(), ville.getLibelleAcheminement(), ville.getLigne_5() } };
+
+		boolean conditionSQL = false;
+		for (int i = 1; i < attributs[0].length; i++) {
+			if (attributs[1][i] != null) {
+				if (conditionSQL) {
+					requete += ", ";
+				}
+				requete += attributs[0][i] + "='" + attributs[1][i] + "'";
+				conditionSQL = true;
+			}
+		}
+
+		return executeRequete(requete + requeteWhere);
+	}
+
+	// DELETE toutes les villes dont les caracteristiques correspondent à celle
+	// donnée en paramètre
+
+	@Override
+	public boolean deleteVille(Ville ville) {
+		String requete = "DELETE FROM ville_france";
+		String requeteWhere = " WHERE ";
+
+		String[][] attributs = { ATTRIBUTS_VILLE,
+				{ ville.getCodeCommune(), ville.getNomCommune(), ville.getCodePostal(), ville.getLatitude(),
+						ville.getLongitude(), ville.getLibelleAcheminement(), ville.getLigne_5() } };
+
+		// Creation String condition
+		boolean conditionSQL = false;
+		for (int i = 0; i < attributs[0].length; i++) {
+			if (attributs[1][i] != null) {
+				if (conditionSQL) {
+					requeteWhere += "AND ";
+				}
+				requeteWhere += attributs[0][i] + "='" + attributs[1][i] + "' ";
+				conditionSQL = true;
+			}
+		}
+
+		// Assemblage des Strings requete
+		String requeteFinale = requete;
+		if (conditionSQL) {
+			requeteFinale += requeteWhere;
+		}
+
+		return executeRequete(requeteFinale);
+
+	}
+
+	@SuppressWarnings("deprecation")
+	private ArrayList<Ville> getVilleWithRequete(String requete) {
+		ArrayList<Ville> villes = new ArrayList<Ville>();
 		Connection con = JDBCConfiguration.getConnection();
 
 		try (Statement stmt = con.createStatement(); ResultSet rs = stmt.executeQuery(requete);) {
@@ -53,101 +179,20 @@ public class VilleDAOImpl implements VilleDAO {
 	}
 
 	@SuppressWarnings("deprecation")
-	@Override
-	public ArrayList<Ville> findSomeVilles(Ville ville) {
-		ArrayList<Ville> villes = new ArrayList<Ville>();
-
-		String requete = "SELECT * FROM ville_france";
-		String requeteWhere = " WHERE ";
+	private boolean executeRequete(String requete) {
+		boolean booleanRequete;
 		Connection con = JDBCConfiguration.getConnection();
-		String[][] attributs = { ATTRIBUTS_VILLE,
-				{ ville.getCodeCommune(), ville.getNomCommune(), ville.getCodePostal(), ville.getLatitude(),
-						ville.getLongitude(), ville.getLibelleAcheminement(), ville.getLigne_5() } };
-
-		// Creation String condition
-		boolean conditionSQL = false;
-		for (int i = 0; i < attributs[0].length; i++) {
-			if (attributs[1][i] != null) {
-				if (conditionSQL) {
-					requeteWhere += "AND ";
-				}
-				requeteWhere += attributs[0][i] + "='" + attributs[1][i] + "' ";
-				conditionSQL = true;
-			}
-		}
-
-		// Assemblage des Strings requete
-		String requeteFinale = requete;
-		if (conditionSQL) {
-			requeteFinale += requeteWhere;
-		}
-
-		try (Statement stmt = con.createStatement(); ResultSet rs = stmt.executeQuery(requeteFinale);) {
-			while (rs.next()) {
-				Ville ville1 = new Ville();
-				ville1.setCodeCommune(rs.getString("code_commune_insee"));
-				ville1.setNomCommune(rs.getString("nom_commune"));
-				ville1.setCodePostal(rs.getString("code_postal"));
-				ville1.setLatitude(rs.getString("latitude"));
-				ville1.setLongitude(rs.getString("longitude"));
-				ville1.setLibelleAcheminement(rs.getString("libelle_acheminement"));
-				ville1.setLigne_5(rs.getString("ligne_5"));
-				villes.add(ville1);
-			}
-		} catch (SQLException e) {
-			logger.log(Priority.ERROR, "Error, couldn't getBis ville.");
-		} finally {
-			try {
-				con.close();
-			} catch (SQLException e) {
-				logger.log(Priority.ERROR, "Error, couldn't close con2.");
-			}
-		}
-		return villes;
-	}
-
-	@SuppressWarnings("deprecation")
-	@Override
-	public boolean addVille(Ville ville) {
-
-		String requete = "INSERT INTO ville_france (";
-		Connection con = JDBCConfiguration.getConnection();
-		boolean booleanRequete = false;
-
-		for (String attribut : ATTRIBUTS_VILLE) {
-			requete += "" + attribut + ",";
-		}
-		requete = requete.substring(0, requete.length() - 1);
-		requete += ") VALUES (";
-
-		if (ville.getCodeCommune() != null) {
-			requete += "'" + ville.getCodeCommune() + "',";
-		} else {
-			requete += "'NULL" + Math.round(10000 * Math.random()) + "',"; // En tant que clef primaire on ne peut pas
-																			// donner la même valeur à chaque fois.
-		}
-
-		requete += checkNull(ville.getNomCommune());
-		requete += checkNull(ville.getCodePostal());
-		requete += checkNull(ville.getLatitude());
-		requete += checkNull(ville.getLongitude());
-		requete += checkNull(ville.getLibelleAcheminement());
-		requete += checkNull(ville.getLigne_5());
-
-		requete = requete.substring(0, requete.length() - 1);
-		requete += ")";
-
 		try (Statement stmt = con.createStatement();) {
 			stmt.executeUpdate(requete);
 			booleanRequete = true;
 		} catch (SQLException e) {
 			booleanRequete = false;
-			logger.log(Priority.ERROR, "Error, couldn't add ville.");
+			logger.log(Priority.ERROR, "");
 		} finally {
 			try {
 				con.close();
 			} catch (SQLException e) {
-				logger.log(Priority.ERROR, "Error, couldn't close con3.");
+				logger.log(Priority.ERROR, "Error, couldn't close con.");
 			}
 		}
 		return booleanRequete;
@@ -160,97 +205,6 @@ public class VilleDAOImpl implements VilleDAO {
 			return "'NULL',";
 		}
 
-	}
-
-	// Modifie la ville dont le numero de commune est donné dans la ville en
-	// parametre et lui donnes les autres parametres donnés dans la ville.
-
-	@SuppressWarnings("deprecation")
-	@Override
-	public boolean modifVille(Ville ville) {
-		boolean booleanRequete = false;
-		String requete = "UPDATE ville_france SET ";
-		String requeteWhere = " WHERE Code_commune_INSEE = '" + ville.getCodeCommune() + "'";
-
-		String[][] attributs = { ATTRIBUTS_VILLE,
-				{ ville.getCodeCommune(), ville.getNomCommune(), ville.getCodePostal(), ville.getLatitude(),
-						ville.getLongitude(), ville.getLibelleAcheminement(), ville.getLigne_5() } };
-
-		Connection con = JDBCConfiguration.getConnection();
-
-		boolean conditionSQL = false;
-		for (int i = 1; i < attributs[0].length; i++) {
-			if (attributs[1][i] != null) {
-				if (conditionSQL) {
-					requete += ", ";
-				}
-				requete += attributs[0][i] + "='" + attributs[1][i] + "'";
-				conditionSQL = true;
-			}
-		}
-		try (Statement stmt = con.createStatement();) {
-			stmt.executeUpdate(requete + requeteWhere);
-			booleanRequete = true;
-		} catch (SQLException e) {
-			booleanRequete = false;
-			logger.log(Priority.ERROR, "Error, couldn't modify ville.");
-		} finally {
-			try {
-				con.close();
-			} catch (SQLException e) {
-				logger.log(Priority.ERROR, "Error, couldn't close con4.");
-			}
-		}
-		return booleanRequete;
-	}
-
-	// DELETE toutes les villes dont les caracteristiques correspondent à celle
-	// donnée en paramètre
-	@SuppressWarnings("deprecation")
-	@Override
-	public boolean deleteVille(Ville ville) {
-		boolean booleanRequete = false;
-		String requete = "DELETE FROM ville_france";
-		String requeteWhere = " WHERE ";
-
-		String[][] attributs = { ATTRIBUTS_VILLE,
-				{ ville.getCodeCommune(), ville.getNomCommune(), ville.getCodePostal(), ville.getLatitude(),
-						ville.getLongitude(), ville.getLibelleAcheminement(), ville.getLigne_5() } };
-
-		Connection con = JDBCConfiguration.getConnection();
-
-		// Creation String condition
-		boolean conditionSQL = false;
-		for (int i = 0; i < attributs[0].length; i++) {
-			if (attributs[1][i] != null) {
-				if (conditionSQL) {
-					requeteWhere += "AND ";
-				}
-				requeteWhere += attributs[0][i] + "='" + attributs[1][i] + "' ";
-				conditionSQL = true;
-			}
-		}
-
-		// Assemblage des Strings requete
-		String requeteFinale = requete;
-		if (conditionSQL) {
-			requeteFinale += requeteWhere;
-		}
-
-		try (Statement stmt = con.createStatement();) {
-			stmt.executeUpdate(requeteFinale);
-			booleanRequete = true;
-		} catch (SQLException e) {
-			booleanRequete = false;
-			logger.log(Priority.ERROR, "Error, couldn't delete ville.");
-		} finally {
-			try {
-				con.close();
-			} catch (SQLException e) {
-				logger.log(Priority.ERROR, "Error, couldn't close con5.");
-			}
-		}
-		return booleanRequete;
 	}
 
 }
